7073941fce01ec82648608dfd318089837b9af6a
ziyang <ziyang0621@gmail.com>
2014-12-05 14:52:15 -0800

    Initial Commit

---
+++ CircleTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleTransition
+//
+//  Created by Ziyang Tan on 12/5/14.
+//  Copyright (c) 2014 Ziyang Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
